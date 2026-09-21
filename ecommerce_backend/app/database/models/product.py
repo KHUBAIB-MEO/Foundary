acf8112a2b5e395e1c7fa3b66765664e5f921211
KHUBAIB-MEO <khubaibnaeem42@gmail.com>
2026-09-22 04:51:09 +0500

    update product model

--- ecommerce_backend/app/database/models/product.py
+++ ecommerce_backend/app/database/models/product.py
@@ -9,12 +9,13 @@ class Product(Base):
     __tablename__ = "products"
 
     id = Column(UUID(as_uuid = True), primary_key = True, default = uuid.uuid4)
+    seller_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable = False)
     description = Column(Text, nullable = True)
     title = Column(Text, nullable = False)
     price = Column(Numeric(10, 2), nullable = False)
     image_url = Column(String(500), nullable = False)
-    file_type = Column(String, nullable = False)
-    file_name = Column(String, nullable = False)
+    file_type = Column(String(100), nullable = False)
+    file_name = Column(String(200), nullable = False)
     stock = Column(Integer, nullable = False, default = 0)
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
